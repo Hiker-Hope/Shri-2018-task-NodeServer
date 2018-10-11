0eb7ae53a38df85807ae0a9123ed91e29b9d6710
Hope R <nadezhda.lkn@yahoo.com>
2018-10-11 22:15:02 +0300

    Checking progress commit)

--- app.js
+++ app.js
@@ -1,72 +1,72 @@
-const express = require('express')
-const app = express()
-const port = 8000
-const events = require('./events.json')
+    const express = require('express')
+    const app = express()
+    const port = 8000
+    const events = require('./events.json')
 
-// filter events for readability
+    // filter events for readability
 
-const eventsAll = events.events.map(item => item)
-const eventsInfo = events.events.filter(item => item.type == 'info')
-const eventsCritical = events.events.filter(item => item.type == 'critical')
-
-
-const serverStartTime = Date.now()
+    const eventsAll = events.events.map(item => item)
+    const eventsInfo = events.events.filter(item => item.type == 'info')
+    const eventsCritical = events.events.filter(item => item.type == 'critical')
 
 
-app.use(function(req, res, next) {
-    let requestTime = Date.now()
+    const serverStartTime = Date.now()
 
-    let currentTime = (requestTime - serverStartTime) / 1000
 
-    let seconds = parseInt(currentTime % 60)
-    seconds = seconds < 10  
-            ? `0${seconds}`
-            : `${seconds}`
-    let minutes = parseInt(currentTime / 60 % 60)
-    minutes = minutes < 10 
-            ? `0${minutes}`
-            : `${minutes}`
-    let hours = parseInt(currentTime / 60 / 60 % 24) 
-    hours = hours < 10 
-            ? `0${hours}`
-            : `${hours}`
+    app.use(function(req, res, next) {
+        let requestTime = Date.now()
 
-    let currentTimeFormatted = `${hours}:${minutes}:${seconds}`
+        let currentTime = (requestTime - serverStartTime) / 1000
 
-    if (req.url == '/status') {
+        let seconds = parseInt(currentTime % 60)
+        seconds = seconds < 10  
+                ? `0${seconds}`
+                : `${seconds}`
+        let minutes = parseInt(currentTime / 60 % 60)
+        minutes = minutes < 10 
+                ? `0${minutes}`
+                : `${minutes}`
+        let hours = parseInt(currentTime / 60 / 60 % 24) 
+        hours = hours < 10 
+                ? `0${hours}`
+                : `${hours}`
 
-        res.send(`${currentTimeFormatted}`)
-    } else {
-        next()
-    }
-})
+        let currentTimeFormatted = `${hours}:${minutes}:${seconds}`
 
-app.use(function(req, res, next) {
-    if (req.url == '/api/events') {
-        res.send(eventsAll)
-    } else {
-        next()
-    }
-})
+        if (req.url == '/status') {
 
-app.use(function(req, res, next) {
-    if (req.url == '/api/events?type=info') {
-        res.send(eventsInfo)
-    } else {
-        next()
-    }
-})
+            res.send(`${currentTimeFormatted}`)
+        } else {
+            next()
+        }
+    })
 
-app.use(function(req, res, next) {
-    if (req.url == '/api/events?type=critical') {
-        res.send(eventsCritical)
-    } else {
-        next()
-    }
-})
+    app.use(function(req, res, next) {
+        if (req.url == '/api/events') {
+            res.send(eventsAll)
+        } else {
+            next()
+        }
+    })
 
-app.use(function(req, res) {
-    res.send(404, 'Page Not Found')
-})
+    app.use(function(req, res, next) {
+        if (req.url == '/api/events?type=info') {
+            res.send(eventsInfo)
+        } else {
+            next()
+        }
+    })
 
-app.listen(port, () => console.log(`Server is listening on port ${port}.`))
+    app.use(function(req, res, next) {
+        if (req.url == '/api/events?type=critical') {
+            res.send(eventsCritical)
+        } else {
+            next()
+        }
+    })
+
+    app.use(function(req, res) {
+        res.send(404, 'Page Not Found')
+    })
+
+    app.listen(port, () => console.log(`Server is listening on port ${port}.`))
