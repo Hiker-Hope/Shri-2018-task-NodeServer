ed592e1213daacb51bd163279047a4e8d2043e41
Hope R <nadezhda.lkn@yahoo.com>
2018-10-12 12:56:43 +0300

    Refactor and add conditions

--- app.js
+++ app.js
@@ -5,54 +5,42 @@
 
     // filter events for readability
 
-    const eventsAll = events.events.map(item => item)
     const eventsInfo = events.events.filter(item => item.type == 'info')
     const eventsCritical = events.events.filter(item => item.type == 'critical')
-
-
     const serverStartTime = Date.now()
 
+    function formatTime(number) {
+        return number = number < 10 
+                ? `0${number}`
+                : `${number}`
+    }
 
     app.use(function(req, res, next) {
+
         let requestTime = Date.now()
-
         let currentTime = (requestTime - serverStartTime) / 1000
-
         let seconds = parseInt(currentTime % 60)
-        seconds = seconds < 10  
-                ? `0${seconds}`
-                : `${seconds}`
         let minutes = parseInt(currentTime / 60 % 60)
-        minutes = minutes < 10 
-                ? `0${minutes}`
-                : `${minutes}`
         let hours = parseInt(currentTime / 60 / 60 % 24) 
-        hours = hours < 10 
-                ? `0${hours}`
-                : `${hours}`
-
-        let currentTimeFormatted = `${hours}:${minutes}:${seconds}`
+        let currentTimeFormatted = `${formatTime(hours)}:${formatTime(minutes)}:${formatTime(seconds)}`
 
         if (req.url == '/status') {
-
             res.send(`${currentTimeFormatted}`)
         } else {
             next()
         }
     })
 
-    app.use(function(req, res, next) {
-        if (req.url == '/api/events') {
-            res.send(eventsAll)
-        } else {
-            next()
-        }
-    })
+
+    // Костыли на случай запроса обоих типов событий одновременно т__т 
+    // нужно разобраться с условиями в передаваемых параметрах для роутов
 
     app.use(function(req, res, next) {
-        if (req.url == '/api/events?type=info') {
+        if (req.url == '/api/events' || req.url == '/api/events?type=critical:info' || req.url == '/api/events?type=info:critical') {
+            res.send(events.events)
+        } else if (req.url == `/api/events?type=info`) {
             res.send(eventsInfo)
-        } else  if (req.url == '/api/events?type=critical') {
+        } else if (req.url == '/api/events?type=critical') {
             res.send(eventsCritical)
         } else {
             next()
