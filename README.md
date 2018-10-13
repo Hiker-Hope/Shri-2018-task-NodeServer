44f7a4180e0e62866d5bde3fb1cdb8a96f3b2afe
Hope R <nadezhda.lkn@yahoo.com>
2018-10-13 16:13:07 +0300

    Prettify

--- README.md
+++ README.md
@@ -4,17 +4,19 @@
 
 ## Запуск:
 
-- склонировать репозиторий: git clone https://github.com/Hiker-Hope/Shri-2018-task-NodeServer.git
-- npm i 
-- node app.js
-- открыть в браузере:
+-   склонировать репозиторий: git clone https://github.com/Hiker-Hope/Shri-2018-task-NodeServer.git
+-   npm i
+-   node app.js
+-   открыть в браузере:
+
 1. localhost:8000/api/events - для просмотра всех событий
 2. localhost:8000/api/events?type=info - для обычных событий
-3. localhost:8000/api/events?type=critical  - для критический событий
+3. localhost:8000/api/events?type=critical - для критический событий
 4. localhost:8000/status - для просмотра времени, прошедшего с запуска сервера
 
-Код находится в файле **app.js** 
+Код находится в файле **app.js**
 
-### TODO 
-* вынести фильтр событий в отдельную функцию
-* разобраться с роутингом (параметры, модули)
+### TODO
+
+-   вынести фильтр событий в отдельную функцию
+-   разобраться с роутингом (параметры, модули)
--- app.js
+++ app.js
@@ -1,54 +1,56 @@
-    const express = require('express')
-    const app = express()
-    const port = 8000
-    const events = require('./events.json')
+const express = require('express')
+const app = express()
+const port = 8000
+const events = require('./events.json')
 
-    // filter events for readability
+// filter events for readability
 
-    const eventsInfo = events.events.filter(item => item.type == 'info')
-    const eventsCritical = events.events.filter(item => item.type == 'critical')
-    const serverStartTime = Date.now()
+const eventsInfo = events.events.filter(item => item.type == 'info')
+const eventsCritical = events.events.filter(item => item.type == 'critical')
+const serverStartTime = Date.now()
 
-    function formatTime(number) {
-        return number = number < 10 
-                ? `0${number}`
-                : `${number}`
+function formatTime(number) {
+    return (number = number < 10 ? `0${number}` : `${number}`)
+}
+
+app.use(function(req, res, next) {
+    let requestTime = Date.now()
+    let currentTime = (requestTime - serverStartTime) / 1000
+    let seconds = parseInt(currentTime % 60)
+    let minutes = parseInt((currentTime / 60) % 60)
+    let hours = parseInt((currentTime / 3600) % 24)
+    let currentTimeFormatted = `${formatTime(hours)}:${formatTime(
+        minutes
+    )}:${formatTime(seconds)}`
+
+    if (req.url == '/status') {
+        res.send(`${currentTimeFormatted}`)
+    } else {
+        next()
     }
+})
 
-    app.use(function(req, res, next) {
+// Костыли на случай запроса обоих типов событий одновременно т__т
+// нужно разобраться с условиями в передаваемых параметрах для роутов
 
-        let requestTime = Date.now()
-        let currentTime = (requestTime - serverStartTime) / 1000
-        let seconds = parseInt(currentTime % 60)
-        let minutes = parseInt(currentTime / 60 % 60)
-        let hours = parseInt(currentTime / 3600 % 24) 
-        let currentTimeFormatted = `${formatTime(hours)}:${formatTime(minutes)}:${formatTime(seconds)}`
+app.use(function(req, res, next) {
+    if (
+        req.url == '/api/events' ||
+        req.url == '/api/events?type=critical:info' ||
+        req.url == '/api/events?type=info:critical'
+    ) {
+        res.send(events.events)
+    } else if (req.url == `/api/events?type=info`) {
+        res.send(eventsInfo)
+    } else if (req.url == '/api/events?type=critical') {
+        res.send(eventsCritical)
+    } else {
+        next()
+    }
+})
 
-        if (req.url == '/status') {
-            res.send(`${currentTimeFormatted}`)
-        } else {
-            next()
-        }
-    })
+app.use(function(req, res) {
+    res.send(404, 'Page Not Found')
+})
 
-
-    // Костыли на случай запроса обоих типов событий одновременно т__т 
-    // нужно разобраться с условиями в передаваемых параметрах для роутов
-
-    app.use(function(req, res, next) {
-        if (req.url == '/api/events' || req.url == '/api/events?type=critical:info' || req.url == '/api/events?type=info:critical') {
-            res.send(events.events)
-        } else if (req.url == `/api/events?type=info`) {
-            res.send(eventsInfo)
-        } else if (req.url == '/api/events?type=critical') {
-            res.send(eventsCritical)
-        } else {
-            next()
-        }
-    })
-
-    app.use(function(req, res) {
-        res.send(404, 'Page Not Found')
-    })
-
-    app.listen(port, () => console.log(`Server is listening on port ${port}.`))
+app.listen(port, () => console.log(`Server is listening on port ${port}.`))
