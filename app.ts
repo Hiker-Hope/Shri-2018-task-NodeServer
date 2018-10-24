3643cbc0b60411327a185b29dadc54ece43be0c9
Hope R <nadezhda.lkn@yahoo.com>
2018-10-24 18:20:40 +0300

    Switch to typescript

--- app.ts
+++ app.ts
@@ -1,26 +1,27 @@
 const express = require('express')
 const app = express()
-const port = 8000
+const port : number = 8000
 const events = require('./events.json')
 
-const serverStartTime = Date.now()
 
-function formatTime(number) {
+let serverStartTime : number = Date.now()
+
+function formatTime(number: number | string) : string{
     return (number = number < 10 ? `0${number}` : `${number}`)
 }
 
 app.use(function(req, res, next) {
-    let requestTime = Date.now()
-    let currentTime = (requestTime - serverStartTime) / 1000
-    let seconds = parseInt(currentTime % 60)
-    let minutes = parseInt((currentTime / 60) % 60)
-    let hours = parseInt((currentTime / 3600) % 24)
-    let currentTimeFormatted = `${formatTime(hours)}:${formatTime(
+    let requestTime : number = Date.now()
+    let currentTime : number = (requestTime - serverStartTime) / 1000
+    let seconds : number = parseInt((currentTime % 60).toString())
+    let minutes : number = parseInt(((currentTime / 60) % 60).toString())
+    let hours : number = parseInt(((currentTime / 3600) % 24).toString())
+    let currentTimeFormatted : string = `${formatTime(hours)}:${formatTime(
         minutes
     )}:${formatTime(seconds)}`
 
     if (req.url == '/status') {
-        res.send(`${currentTimeFormatted}`)
+        res.send(`${currentTimeFormatted}`)   
     } else {
         next()
     }
@@ -28,22 +29,22 @@ app.use(function(req, res, next) {
 
 // Массив с корректными типами событий для запроса
 
-const valid_events_types = ['critical', 'info']
+let valid_events_types : string[] = ['critical', 'info']
 
 //  По типу, переданному в запросе, выдаем либо все, либо отфильтрованные события
 
-function sendEvents(res, type) {
-    const input = events
+function sendEvents(res : Response, type : string) : void {
+    const input : {} = events
     if (!type) {
         res.send(input)
         return
     }
-    const types = type.split(':')
+    const types : string[] = type.split(':')
 
     if (types.some(item => valid_events_types.indexOf(item) == -1)) {
-        res.status(400).send('Incorrect type')
+        res.send(400, 'Incorrect type')
     } else {
-        filteredEvents = input.events.filter(
+        let filteredEvents : string[] = input.events.filter(
             event => types.indexOf(event.type) != -1
         )
         res.send(filteredEvents)
@@ -51,17 +52,18 @@ function sendEvents(res, type) {
 }
 
 app.get('/api/events', (req, res) => {
-    const type = req.query.type
+    const type : string = req.query.type
     sendEvents(res, type)
 })
 
 app.post('/api/events', (req, res) => {
-    const type = req.body.type
+    const type : string = req.body.type
     sendEvents(res, type)
 })
 
 app.use(function(req, res) {
-    res.send(404, 'Page Not Found')
+    res.status(404).send('Page Not Found')
 })
 
 app.listen(port, () => console.log(`Server is listening on port ${port}.`))
+
---
+++ bundle.js
@@ -0,0 +1,53 @@
+"use strict";
+var express = require('express');
+var app = express();
+var port = 8000;
+var events = require('./events.json');
+var serverStartTime = Date.now();
+function formatTime(number) {
+    return (number = number < 10 ? "0" + number : "" + number);
+}
+app.use(function (req, res, next) {
+    var requestTime = Date.now();
+    var currentTime = (requestTime - serverStartTime) / 1000;
+    var seconds = parseInt((currentTime % 60).toString());
+    var minutes = parseInt(((currentTime / 60) % 60).toString());
+    var hours = parseInt(((currentTime / 3600) % 24).toString());
+    var currentTimeFormatted = formatTime(hours) + ":" + formatTime(minutes) + ":" + formatTime(seconds);
+    if (req.url == '/status') {
+        res.send("" + currentTimeFormatted);
+    }
+    else {
+        next();
+    }
+});
+// Массив с корректными типами событий для запроса
+var valid_events_types = ['critical', 'info'];
+//  По типу, переданному в запросе, выдаем либо все, либо отфильтрованные события
+function sendEvents(res, type) {
+    var input = events;
+    if (!type) {
+        res.send(input);
+        return;
+    }
+    var types = type.split(':');
+    if (types.some(function (item) { return valid_events_types.indexOf(item) == -1; })) {
+        res.status(400).send('Incorrect type');
+    }
+    else {
+        var filteredEvents = input.events.filter(function (event) { return types.indexOf(event.type) != -1; });
+        res.send(filteredEvents);
+    }
+}
+app.get('/api/events', function (req, res) {
+    var type = req.query.type;
+    sendEvents(res, type);
+});
+app.post('/api/events', function (req, res) {
+    var type = req.body.type;
+    sendEvents(res, type);
+});
+app.use(function (req, res) {
+    res.status(404).send('Page Not Found');
+});
+app.listen(port, function () { return console.log("Server is listening on port " + port + "."); });
